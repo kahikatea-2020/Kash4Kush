1447fe403ca06eb4e0a966171e67eeac24b17506
Pat-Lim <impatlim@gmail.com>
2020-04-24 10:36:40 +1200

    rebasee

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import FeaturedStrain from './FeaturedStrain'
 import Home from './Home'
 import Profile from './Profile'
 import Cart from './Cart'
 
-const App = () => {
-  return (
-    <>
-      <Header/>
-      <Home/>
-      <Profile />
-      <Cart />
-      <Footer />
-    </>
-  )
-}
+class App extends React.Component {
+//   this.state = {
 
+  // }
+
+  render () {
+    return (
+      <>
+            <Header />
+            <Home />
+            {/* <Profile /> */}
+            {/* <Cart /> */}
+            <Footer />
+      </>
+    )
+  }
+}
 export default App
