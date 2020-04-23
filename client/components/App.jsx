bd88f1685f39642831bfa0c3afe2bcaa0fde6ef9
Pat-Lim <impatlim@gmail.com>
2020-04-24 11:20:12 +1200

    fix merge conflicts

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,7 +5,6 @@ import Home from './Home'
 import Profile from './Profile'
 import Cart from './Cart'
 
-<<<<<<< HEAD
 class App extends React.Component {
 //   this.state = {
 
@@ -23,53 +22,5 @@ class App extends React.Component {
     )
   }
 }
-||||||| merged common ancestors
-const App = () => {
-  return (
-    <>
-      <Header/>
-<<<<<<< HEAD
-      <Home/>
-      {/* <Profile /> */}
-      {/* <Cart /> */}
-      {/* <Footer /> */}
-||||||| merged common ancestors
-      {/* <Home/> */}
-      {/* <Profile /> */}
-      <Cart />
-      {/* <Footer /> */}
-=======
-      <Home/>
-      <Profile />
-      <Cart />
-      <Footer />
->>>>>>> 4c460ac921baeaeb023e47a73d78f4cb7111df0f
-    </>
-  )
-}
-=======
-class App extends React.Component {
-//   this.state = {
 
-  // }
->>>>>>> 1c71cafb5185cf3b69342bfbfb474dc4b00880d4
-
-<<<<<<< HEAD
 export default App
-||||||| merged common ancestors
-export default App
-=======
-  render () {
-    return (
-      <>
-            <Header />
-            <Home />
-            {/* <Profile /> */}
-            {/* <Cart /> */}
-            <Footer />
-      </>
-    )
-  }
-}
-export default App
->>>>>>> 1c71cafb5185cf3b69342bfbfb474dc4b00880d4
